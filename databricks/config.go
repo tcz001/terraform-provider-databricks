cfe5f28e2ad03df11478795112c65b015dc37505
tcz001 <fan.torchz@gmail.com>
2020-04-19 17:08:11 -0700

    [fix #1, fix #2]support azure sp credentials and token resource

--- databricks/config.go
+++ databricks/config.go
@@ -3,8 +3,11 @@ package databricks
 import (
 	"time"
 
+	"github.com/Azure/go-autorest/autorest/adal"
+	azAuth "github.com/Azure/go-autorest/autorest/azure/auth"
 	"github.com/tcz001/databricks-sdk-go/api/clusters"
 	secrets "github.com/tcz001/databricks-sdk-go/api/secrets"
+	token "github.com/tcz001/databricks-sdk-go/api/token"
 	"github.com/tcz001/databricks-sdk-go/api/workspace"
 	apiClient "github.com/tcz001/databricks-sdk-go/client"
 )
@@ -15,26 +18,63 @@ const (
 )
 
 type Config struct {
-	Domain                              *string
-	Token                               *string
-	XDatabricksAzureWorkspaceResourceId *string
-	XDatabricksAzureSPManagementToken   *string
+	Domain      *string
+	Token       *string
+	WorkspaceId *string
+	AzCCC       *azAuth.ClientCredentialsConfig
+	DBCCC       *azAuth.ClientCredentialsConfig
 }
 
 type Client struct {
 	clusters  *clusters.Endpoint
 	workspace *workspace.Endpoint
 	secrets   *secrets.Endpoint
+	token     *token.Endpoint
+}
+
+func ServicePrincipalToken(ccc *azAuth.ClientCredentialsConfig) (*adal.ServicePrincipalToken, error) {
+	oauthConfig, err := adal.NewOAuthConfigWithAPIVersion(ccc.AADEndpoint, ccc.TenantID, nil)
+	if err != nil {
+		return nil, err
+	}
+	return adal.NewServicePrincipalToken(*oauthConfig, ccc.ClientID, ccc.ClientSecret, ccc.Resource)
 }
 
 func (c *Config) Client() (interface{}, error) {
 	var client Client
 
+	var xDatabricksAzureSPManagementToken *string
+	if c.DBCCC != nil && c.AzCCC != nil {
+		// Get AZ Databricks Token
+		rsToken, err := ServicePrincipalToken(c.DBCCC)
+		if err != nil {
+			return nil, err
+		}
+		err = rsToken.EnsureFresh()
+		if err != nil {
+			return nil, err
+		}
+		rsOAuthToken := rsToken.OAuthToken()
+		c.Token = &rsOAuthToken
+
+		// Get Az Management SP Token
+		azToken, err := ServicePrincipalToken(c.AzCCC)
+		if err != nil {
+			return nil, err
+		}
+		err = azToken.EnsureFresh()
+		if err != nil {
+			return nil, err
+		}
+		oauthToken := azToken.OAuthToken()
+		xDatabricksAzureSPManagementToken = &oauthToken
+	}
+
 	opts := apiClient.Options{
 		Domain:                              c.Domain,
 		Token:                               c.Token,
-		XDatabricksAzureSPManagementToken:   c.XDatabricksAzureSPManagementToken,
-		XDatabricksAzureWorkspaceResourceId: c.XDatabricksAzureWorkspaceResourceId,
+		XDatabricksAzureSPManagementToken:   xDatabricksAzureSPManagementToken,
+		XDatabricksAzureWorkspaceResourceId: c.WorkspaceId,
 		MaxRetries:                          maxRetries,
 		RetryDelay:                          retryDelay,
 	}
@@ -46,6 +86,7 @@ func (c *Config) Client() (interface{}, error) {
 	client.clusters = &clusters.Endpoint{Client: cl}
 	client.workspace = &workspace.Endpoint{Client: cl}
 	client.secrets = &secrets.Endpoint{Client: cl}
+	client.token = &token.Endpoint{Client: cl}
 
 	return &client, nil
 }
--- databricks/provider.go
+++ databricks/provider.go
@@ -2,6 +2,8 @@ package databricks
 
 import (
 	"github.com/hashicorp/terraform/helper/schema"
+
+	azAuth "github.com/Azure/go-autorest/autorest/azure/auth"
 )
 
 func Provider() *schema.Provider {
@@ -15,12 +17,40 @@ func Provider() *schema.Provider {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
+
+			"client_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("ARM_CLIENT_ID", ""),
+				Description: "The Client ID which should be used.",
+			},
+
+			"tenant_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("ARM_TENANT_ID", ""),
+				Description: "The Tenant ID which should be used.",
+			},
+			// Client Secret specific fields
+			"client_secret": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("ARM_CLIENT_SECRET", ""),
+				Description: "The Client Secret which should be used. For use When authenticating as a Service Principal using a Client Secret.",
+			},
+
+			"workspace_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The databricks workspace id",
+			},
 		},
 		ResourcesMap: map[string]*schema.Resource{
 			"databricks_cluster":      resourceDatabricksCluster(),
 			"databricks_notebook":     resourceDatabricksNotebook(),
 			"databricks_secret_scope": resourceDatabricksSecretScope(),
 			"databricks_secret":       resourceDatabricksSecret(),
+			"databricks_token":        resourceDatabricksToken(),
 		},
 		ConfigureFunc: providerConfigure,
 	}
@@ -37,6 +67,34 @@ func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 	if token, ok := d.GetOk("token"); ok {
 		s := token.(string)
 		config.Token = &s
+	}
+
+	if workspaceId, ok := d.GetOk("workspace_id"); ok {
+		s := workspaceId.(string)
+		config.WorkspaceId = &s
+	}
+
+	var clientID, clientSecret, tenantID string
+
+	if s, ok := d.GetOk("client_id"); ok {
+		clientID = s.(string)
+	}
+
+	if s, ok := d.GetOk("client_secret"); ok {
+		clientSecret = s.(string)
+	}
+
+	if s, ok := d.GetOk("tenant_id"); ok {
+		tenantID = s.(string)
+	}
+
+	if clientID != "" && clientSecret != "" && tenantID != "" {
+		dbccc := azAuth.NewClientCredentialsConfig(clientID, clientSecret, tenantID)
+		dbccc.Resource = "2ff814a6-3304-4ab8-85cb-cd0e6f879c1d"
+		azccc := azAuth.NewClientCredentialsConfig(clientID, clientSecret, tenantID)
+		azccc.Resource = "https://management.core.windows.net/"
+		config.DBCCC = &dbccc
+		config.AzCCC = &azccc
 	}
 
 	return config.Client()
